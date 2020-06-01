9e641e55ec1353d77a0627db334b2f77c386ee77
Dan Toloudis <dmt@completelydifferent.com>
2020-06-01 07:36:30 -0700

    add code to allocate mesh memory, need to pass queue/commandpool in

--- src/graphicsvk/meshVk.cpp
+++ src/graphicsvk/meshVk.cpp
@@ -1,5 +1,7 @@
 #include "meshVk.h"
 
+#include "VulkanDevice.hpp"
+
 MeshVk::MeshVk(vks::VulkanDevice* vulkanDevice,
                uint32_t i_nVertices,
                const float* i_Vertices,
@@ -9,6 +11,69 @@ MeshVk::MeshVk(vks::VulkanDevice* vulkanDevice,
                const uint32_t* i_Indices)
 {
   m_device = vulkanDevice;
+
+  const VkDeviceSize vertexBufferSize = i_nVertices * sizeof(float) * 3;
+  const VkDeviceSize indexBufferSize = i_nIndices * sizeof(uint32_t);
+
+  VkBuffer stagingBuffer;
+  VkDeviceMemory stagingMemory;
+
+  VkCommandBuffer copyCmd = vulkanDevice->createCommandBuffer(VK_COMMAND_BUFFER_LEVEL_PRIMARY, commandPool);
+  VkCommandBufferBeginInfo cmdBufInfo = vks::initializers::commandBufferBeginInfo();
+
+  // Copy input data to VRAM using a staging buffer
+  {
+    // Vertices
+    VK_CHECK_RESULT(
+      vulkanDevice->createBuffer(VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                                 VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                                 vertexBufferSize,
+                                 &stagingBuffer,
+                                 &stagingMemory,
+                                 i_Vertices));
+
+    VK_CHECK_RESULT(vulkanDevice->createBuffer(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+                                               VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                                               vertexBufferSize,
+                                               &vertexBuffer,
+                                               &vertexMemory));
+
+    VK_CHECK_RESULT(vkBeginCommandBuffer(copyCmd, &cmdBufInfo));
+    VkBufferCopy copyRegion = {};
+    copyRegion.size = vertexBufferSize;
+    vkCmdCopyBuffer(copyCmd, stagingBuffer, vertexBuffer, 1, &copyRegion);
+    VK_CHECK_RESULT(vkEndCommandBuffer(copyCmd));
+
+    submitWork(copyCmd, queue);
+
+    vkDestroyBuffer(device, stagingBuffer, nullptr);
+    vkFreeMemory(device, stagingMemory, nullptr);
+
+    // Indices
+    VK_CHECK_RESULT(
+      vulkanDevice->createBuffer(VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                                 VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                                 indexBufferSize,
+                                 &stagingBuffer,
+                                 &stagingMemory,
+                                 i_Indices));
+
+    VK_CHECK_RESULT(vulkanDevice->createBuffer(VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+                                               VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                                               indexBufferSize,
+                                               &indexBuffer,
+                                               &indexMemory));
+
+    VK_CHECK_RESULT(vkBeginCommandBuffer(copyCmd, &cmdBufInfo));
+    copyRegion.size = indexBufferSize;
+    vkCmdCopyBuffer(copyCmd, stagingBuffer, indexBuffer, 1, &copyRegion);
+    VK_CHECK_RESULT(vkEndCommandBuffer(copyCmd));
+
+    submitWork(copyCmd, queue);
+
+    vkDestroyBuffer(device, stagingBuffer, nullptr);
+    vkFreeMemory(device, stagingMemory, nullptr);
+  }
 }
 
 BoundingBox
