f6bd7e549921de88079a1ec271456cf388d3e666
Dan <danielt@alleninstitute.org>
2020-07-14 15:18:40 -0700

    clang-format, change a color

--- src/graphicsvk/sceneRendererVk.cpp
+++ src/graphicsvk/sceneRendererVk.cpp
@@ -64,7 +64,7 @@ SceneRendererVk::render(RenderTarget* target, const Camera& camera, const Scene&
 
     // one per attachment
     VkClearValue clearValues[2];
-    clearValues[0].color = { { 1.0f, 0.0f, 0.0f, 1.0f } };
+    clearValues[0].color = { { 0.0f, 1.0f, 0.0f, 1.0f } };
     clearValues[1].depthStencil = { 1.0f, 0 };
 
     VkRenderPassBeginInfo renderPassBeginInfo = {};
